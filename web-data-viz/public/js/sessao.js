439efa9edca16471ed9c2a0d3b1b4c55de5facbe
ManuellaArantes <manuella.arantes@sptech.school>
2026-05-01 16:56:50 -0300

    Alerta na página quiz redirecionando para o login

--- web-data-viz/public/js/sessao.js
+++ web-data-viz/public/js/sessao.js
@@ -9,6 +9,60 @@ function obterCaminhoLogin() {
     return "./login.html";
 }
 
+function obterCaminhoPagina(pagina) {
+    let caminhoAtual = window.location.pathname;
+    let estaNaDashboard = caminhoAtual.indexOf("/dashboard/") > -1;
+
+    if (estaNaDashboard) {
+        return "../" + pagina;
+    }
+
+    return "./" + pagina;
+}
+
+function usuarioEstaLogado() {
+    let email = sessionStorage.EMAIL_USUARIO;
+    let nome = sessionStorage.NOME_USUARIO;
+
+    return email != null && nome != null;
+}
+
+function atualizarCabecalho() {
+    let linkDashboard = document.querySelector(".link-dashboard");
+    let linkAuth = document.querySelector(".link-auth");
+    let nomeUsuario = document.querySelector(".nome-usuario-header");
+    let usuarioLogado = usuarioEstaLogado();
+
+    if (linkDashboard) {
+        linkDashboard.href = obterCaminhoPagina("dashboard/cards.html");
+
+        if (usuarioLogado) {
+            linkDashboard.style.display = "inline-flex";
+        } else {
+            linkDashboard.style.display = "none";
+        }
+    }
+
+    if (linkAuth) {
+        if (usuarioLogado) {
+            linkAuth.textContent = "DESLOGAR";
+            linkAuth.href = "#";
+            linkAuth.onclick = function () {
+                limparSessao();
+                return false;
+            };
+        } else {
+            linkAuth.textContent = "SUBSCRIBE";
+            linkAuth.href = obterCaminhoPagina("cadastro.html");
+            linkAuth.onclick = null;
+        }
+    }
+
+    if (nomeUsuario && usuarioLogado) {
+        nomeUsuario.textContent = sessionStorage.NOME_USUARIO;
+    }
+}
+
 function validarSessao() {
     let email = sessionStorage.EMAIL_USUARIO;
     let nome = sessionStorage.NOME_USUARIO;
@@ -53,3 +107,7 @@ function finalizarAguardar(texto) {
         div_erros_login.innerHTML = texto;
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    atualizarCabecalho();
+});
